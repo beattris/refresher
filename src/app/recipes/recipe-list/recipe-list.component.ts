77dc959a6ce3d818599834778b8b91bf4ce85480
beattris <beatriceebirim@gmail.com>
2024-04-17 09:05:17 -0700

    used ngFor to output the recipes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'This is just a test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkvvy9uOBJkewW7zbTGMr03B81f5klHJ10qQ&s')
+  ];
 }
