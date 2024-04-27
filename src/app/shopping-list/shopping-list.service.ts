3d2bc50951c630f01d872458b607399a973758c6
beattris <beatriceebirim@gmail.com>
2024-04-27 06:08:00 -0700

    modified the shopping list service

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,9 +1,26 @@
 // import { Injectable } from '@angular/core';
 
+import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
+
 // @Injectable({
 //   providedIn: 'root'
 // })
 export class ShoppingListService {
-  
+  ingredientsChanged = new EventEmitter<Ingredient[]>();
+  private ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10),
+  ];
   constructor() { }
+
+  getIngredients(){
+    return this.ingredients.slice();
+  }
+
+  addIngredient(ingredient: Ingredient){
+    this.ingredients.push(ingredient);
+    // USED TO ADD THE INGREDIENTS PROPERLY
+    this.ingredientsChanged.emit(this.ingredients.slice());
+  }
 }
